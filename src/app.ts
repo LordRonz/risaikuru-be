75fea15cfcfbe932f60dd5393a4fa0000df4ec4f
LordRonz <aarontanhar2000@gmail.com>
2022-03-26 11:07:18 +0700

    feat: add buffer route ffs :rocket:

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import cors from 'cors';
 import express from 'express';
-import fileUpload from 'express-fileupload';
 
 import routes from './routes';
 
 const app = express();
+app.use(express.json());
 app.use(cors());
-app.use(fileUpload());
 
 app.use('/', routes);
 
--- src/routes/classify.route.ts
+++ src/routes/classify.route.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
+import fileUpload from 'express-fileupload';
 
 import classifyImage from '../controller/classify.controller';
 import classifyImageBuffer from '../controller/classifyBuffer.controller';
 
 const router = Router();
 
-router.post('/', classifyImage);
+router.post('/', fileUpload(), classifyImage);
 router.post('/buffer', classifyImageBuffer);
 
 export default router;
